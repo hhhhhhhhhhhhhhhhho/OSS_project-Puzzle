18d6c0784c69d7e4b189e19976aa586fd3096ad3
allstar94 <allstar94@naver.com>
2019-11-17 17:46:32 +0900

    massage

---
+++ AndroidPuzzleGame/app/src/main/java/dragosholban/com/androidpuzzlegame/HintWindow.java
@@ -0,0 +1,19 @@
+package dragosholban.com.androidpuzzlegame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+//taehoon git
+public class HintWindow extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_now);
+    }
+
+    public void onbackButtonCliked(View v){
+        Toast.makeText(getApplicationContext(), "퍼즐 화면으로 돌아갑니다.", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
