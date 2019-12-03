877b158034cfc8503a8ac0668f10543b8090a5be
hhhhhhhhhhhhhhhhho <man2719@naver.com>
2019-12-03 16:23:19 +0900

    로컬 저장소 그대로 복사 함

--- AndroidPuzzleGame/app/src/main/java/dragosholban/com/androidpuzzlegame/HintWindow.java
+++ AndroidPuzzleGame/app/src/main/java/dragosholban/com/androidpuzzlegame/HintWindow.java
@@ -47,67 +47,13 @@ public class HintWindow extends PuzzleActivity{
         }
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> taejeong
     public static Bitmap rotateImage(Bitmap source, float angle) {
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
                 matrix, true);
-<<<<<<< HEAD
 
     }
-
-
-    private void setPicFromAsset(String assetName, ImageView imageView) {
-        // Get the dimensions of the View
-        int targetW = imageView.getWidth();
-        int targetH = imageView.getHeight();
-=======
-    public void onbackButtonCliked(View v){
-        Toast.makeText(getApplicationContext(), "퍼즐 화면으로 돌아갑니다.", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-
-
-}
->>>>>>> taejeong
-
-        AssetManager am = getAssets();
-        try {
-            InputStream is = am.open("img/" + assetName);
-            // Get the dimensions of the bitmap
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inJustDecodeBounds = true;
-            BitmapFactory.decodeStream(is, new Rect(-1, -1, -1, -1), bmOptions);
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
-
-            // Determine how much to scale down the image
-            int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-
-            is.reset();
-
-            // Decode the image file into a Bitmap sized to fill the View
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inSampleSize = scaleFactor;
-            bmOptions.inPurgeable = true;
-
-            Bitmap bitmap = BitmapFactory.decodeStream(is, new Rect(-1, -1, -1, -1), bmOptions);
-            imageView.setImageBitmap(bitmap);
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-        }
-
-=======
-
-    }
->>>>>>> taejeong
 
     public void onbackButtonCliked(View v){
         Toast.makeText(getApplicationContext(), "퍼즐 화면으로 돌아갑니다.", Toast.LENGTH_SHORT).show();
@@ -115,8 +61,4 @@ public class HintWindow extends PuzzleActivity{
 
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> taejeong
 }
